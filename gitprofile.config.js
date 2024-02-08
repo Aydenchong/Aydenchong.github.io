0f90864cac29899960a382e722997123ae3ac7b8
Ayden <aydenchong9@gmail.com>
2024-02-08 18:25:27 +0800

    updated

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,9 +2,9 @@
 
 const config = {
   github: {
-    username: '', //TODO: Your GitHub org/user name. (Required)
+    username: 'Aydenchong', //TODO: Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 4, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -14,16 +14,16 @@ const config = {
     username: '', //TODO: Your Scratch username. (Required)
     limit: 8, // How many projects to display; maximum 20
     sortBy: 'date', // date | views | remixes
-    corsProxy: 'https://scratchapicors-8nbi5t6td-theyoungmaker.vercel.app/', //TODO: Enter your CORS Proxy here
+    corsProxy: '', //TODO: Enter your CORS Proxy here
   },
   social: {
     //TODO: Add in Socials
-    linkedin: '',
-    facebook: '',
-    instagram: '',
-    tiktok: '',
-    email: '',
-    website: '',
+    // linkedin: '',
+    // facebook: '',
+    // instagram: '',
+    // tiktok: '',
+    email: 'aydenchong9@gmail.com',
+    // website: '',
     // dev: '',
     // twitter: '',
     // mastodon: '',
@@ -48,10 +48,8 @@ const config = {
     'Git',
     'CSS',
     'MakeCode',
-    'ScratchJr',
     'Hopscotch',
     'Robotics',
-    'Typescript',
   ],
   experiences: [
     {
@@ -79,16 +77,16 @@ const config = {
   ],
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Anderson Secondary School',
+      degree: 'O Level',
+      from: '2023',
+      to: 'Present',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Springdale Primary School',
+      degree: 'PSLE',
+      from: '2017',
+      to: '2022',
     },
   ],
 
@@ -127,7 +125,7 @@ const config = {
   },
   themeConfig: {
     //TODO: Change the theme of the website to your liking
-    defaultTheme: 'bumblebee',
+    defaultTheme: 'night',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
